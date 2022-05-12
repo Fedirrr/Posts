ba22943910b8b0cbbf4291b2ed88bee0888280cb
Fedirrr <fedyapavlyshynets@gmail.com>
2022-05-13 00:21:25 +0300

    created project

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install
+To install everything necessary for the project
+```
+npm i
+```
+# Start
+To start a project
+```
+npm start
+```
---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="wrapper">
+            <div className="content"></div>
+        </div>
+    );
+}
+
+export default App;
+
