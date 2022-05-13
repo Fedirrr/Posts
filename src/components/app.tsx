5f8237b3c2be7133954d3faf2e4ebe119bb9d29d
Fedirrr <73940792+Fedirrr@users.noreply.github.com>
2022-05-13 10:49:50 +0300

    Add files via upload

---
+++ src/components/app.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import PostsList from '../PostsList/PostsList';
+import Header from '../Header/Header';
+import './app.css'
+const App: FC = () => {
+    return (
+        <div className="wrapper">
+            <div className="content">
+                <Header/>
+                <PostsList/>
+            </div>
+        </div>
+    );
+};
+
+export default App;
